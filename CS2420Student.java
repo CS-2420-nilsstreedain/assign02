0580dab08566c040972e211bee52367a03667c47
GlutenFreeBees <60480751+GlutenFreeBees@users.noreply.github.com>
2021-02-03 00:10:47 -0700

    Update CS2420Student.java

--- CS2420Student.java
+++ CS2420Student.java
@@ -1,14 +1,49 @@
 package assign02;
 
-public class CS2420Student {
-	/*
-	 * public CS2420Student(String firstName, String lastName, int uNID, EmailAddress contactInfo)
-	 * public EmailAddress getContactInfo()
-	 * public void addScore(double score, String category)
-	 * public double computeFinalScore() 
-	 * public String computeFinalGrade()
-	 */
+import java.util.ArrayList;
+
+public class CS2420Student extends UofUStudent{
+	
+	private EmailAddress contactInfo;
+	private ArrayList<Double> labScores = new ArrayList<>();
+	private ArrayList<Double> assignScores = new ArrayList<>();
+	private ArrayList<Double> quizScores = new ArrayList<>();
+	private ArrayList<Double> examScores = new ArrayList<>();
+	
+	public CS2420Student(String firstName, String lastName, int uNID, EmailAddress contactInfo) {
+		super(firstName,lastName,uNID);
+		contactInfo = this.contactInfo;
+	}
+	
+	public EmailAddress getContactInfo() {
+		return contactInfo;
+	}
+	
+	public void addScore(double score, String category) {
+		switch (category) {
+		case "lab": 
+			labScores.add(score);
+			break;
+		case "assignment": 
+			assignScores.add(score);
+			break;
+		case "quiz": 
+			quizScores.add(score);
+			break;
+		case "exam": 
+			examScores.add(score);
+			break;
+		default:
+			throw new IllegalArgumentException("Category must be lab, assignment, quiz, or exam");
+		}
+	}
 	
 	
+	public double computeFinalScore() {
+		return 0;
+	}
+	public String computeFinalGrade() {
+		return null;
+	}
 	
 }
