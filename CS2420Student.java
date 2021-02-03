f64329d989508ca4a0ebc5f523fa168737672986
Nils Streedain <business@nilsstreedain.com>
2021-02-03 00:22:29 -0700

    Added comments to CS2420Student.java

--- CS2420Student.java
+++ CS2420Student.java
@@ -2,48 +2,90 @@ package assign02;
 
 import java.util.ArrayList;
 
-public class CS2420Student extends UofUStudent{
-	
+/**
+ * This class represents a CS2420 student, in which the uNID cannot change once
+ * the student is created. Note that each student's uNID is unique. contact
+ * information in the form of an email adress is stored along with assignment
+ * scores in several ArrayLists, depending on the assignment category.
+ * 
+ * @author Nils Streedain, Kyle Williams
+ * @version Febuary 2, 2021
+ */
+public class CS2420Student extends UofUStudent {
+
 	private EmailAddress contactInfo;
+
 	private ArrayList<Double> labScores = new ArrayList<>();
 	private ArrayList<Double> assignScores = new ArrayList<>();
 	private ArrayList<Double> quizScores = new ArrayList<>();
 	private ArrayList<Double> examScores = new ArrayList<>();
-	
+
+	/**
+	 * Creates a CS2420 student from the given first name, last name, uNID and Email
+	 * Address.
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param uNID
+	 * @param contactInfo
+	 */
 	public CS2420Student(String firstName, String lastName, int uNID, EmailAddress contactInfo) {
-		super(firstName,lastName,uNID);
+		super(firstName, lastName, uNID);
 		contactInfo = this.contactInfo;
 	}
-	
+
+	/**
+	 * Getter method for the contactInfo field of this CS2420 student object.
+	 * 
+	 * @return this student's contactInfo
+	 */
 	public EmailAddress getContactInfo() {
 		return contactInfo;
 	}
-	
+
+	/**
+	 * Adds the score of an assignment of a certain type to the ArrayList of scores
+	 * for that type. Only accepts categories of type "lab", "assignment", "quiz",
+	 * or "exam".
+	 * 
+	 * @param score
+	 * @param category
+	 */
 	public void addScore(double score, String category) {
 		switch (category) {
-		case "lab": 
+		case "lab":
 			labScores.add(score);
 			break;
-		case "assignment": 
+		case "assignment":
 			assignScores.add(score);
 			break;
-		case "quiz": 
+		case "quiz":
 			quizScores.add(score);
 			break;
-		case "exam": 
+		case "exam":
 			examScores.add(score);
 			break;
 		default:
 			throw new IllegalArgumentException("Category must be lab, assignment, quiz, or exam");
 		}
 	}
-	
-	
+
+	/**
+	 * Undefined
+	 * 
+	 * @return undefined
+	 */
 	public double computeFinalScore() {
 		return 0;
 	}
+
+	/**
+	 * Undefined
+	 * 
+	 * @return undefined
+	 */
 	public String computeFinalGrade() {
 		return null;
 	}
-	
+
 }
