5749b0cf5ddf71face4ab6654c8340682d9d78e4
GlutenFreeBees <60480751+GlutenFreeBees@users.noreply.github.com>
2021-02-03 17:11:16 -0700

    Put code for computefinalscore and the other one

--- CS2420Student.java
+++ CS2420Student.java
@@ -16,7 +16,7 @@ public class CS2420Student extends UofUStudent {
 	private final double LAB_WEIGHT = .10;
 	private final double QUIZ_WEIGHT = .10;
 	private final double EXAM_WEIGHT = .30;
-	private final double ASSIGNEMNT_WEIGHT = .50;
+	private final double ASSIGNMENT_WEIGHT = .50;
 
 	private EmailAddress contactInfo;
 
@@ -81,16 +81,30 @@ public class CS2420Student extends UofUStudent {
 	 * @return undefined
 	 */
 	public double computeFinalScore() {
-		double tempCategoryScore = 0;
-		double finalScoreSum = 0;
-		
-		for (Double score : assignScores) {
-			tempCategoryScore += score;
+		if ((labScores.size() <= 2) || (quizScores.size() <= 2) || (examScores.size() <= 1) || (assignScores.size() <= 1)){
+			return 0;
 		}
-		
-		finalScoreSum += tempCategoryScore * 0;
-		
+		double finalScoreSum = 0;
+		finalScoreSum += averageArray(labScores) * LAB_WEIGHT;
+		finalScoreSum += averageArray(assignScores) * ASSIGNMENT_WEIGHT;
+		finalScoreSum += averageArray(quizScores) * QUIZ_WEIGHT;
+		finalScoreSum += averageArray(examScores) * EXAM_WEIGHT;
+
 		return finalScoreSum;
+	}
+
+	/**
+	 * Undefined
+	 * 
+	 * @return avg
+	 */
+	private double averageArray(ArrayList<Double> array) {
+		double avg = 0;
+		for (double score : array) {
+			avg += score;
+		}
+		avg = avg / array.size();
+		return avg;
 	}
 
 	/**
@@ -99,7 +113,35 @@ public class CS2420Student extends UofUStudent {
 	 * @return undefined
 	 */
 	public String computeFinalGrade() {
-		return null;
+		double score = computeFinalScore();
+		if ((labScores.size() <= 2) || (quizScores.size() <= 2) || (examScores.size() <= 1) || (assignScores.size() <= 1)){
+			return "N/A";
+		}
+		
+		if (score >= 93.0) {
+			return "A";
+		} else if (score >= 90.0) {
+			return "A-";
+		} else if (score >= 87.0) {
+			return "B+";
+		} else if (score >= 83.0) {
+			return "B";
+		} else if (score >= 80.0) {
+			return "B-";
+		} else if (score >= 77.0) {
+			return "C+";
+		} else if (score >= 73.0) {
+			return "C";
+		} else if (score >= 70.0) {
+			return "C-";
+		} else if (score >= 67.0) {
+			return "D+";
+		} else if (score >= 63.0) { 
+			return "D";
+		} else if (score >= 60.0) { 
+			return "D-";
+		} else {
+			return "E";
+		}
 	}
-
 }
